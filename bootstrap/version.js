bd69c6e25d28157e53fcc2384de8c4328074106c
Olivier Breuleux <breuleux@gmail.com>
2015-04-06 17:33:26 -0400

    new bootstrap

--- bootstrap/version.js
+++ bootstrap/version.js
@@ -1,4 +1,4 @@
 
-'use strict';require('earlgrey-runtime/6');let $targ$0;$targ$0="0.0.1";(exports["version"]=$targ$0);[];
+'use strict';require('earlgrey-runtime/6');let $targ$0;$targ$0="0.0.1";(exports["version"]=$targ$0);(void 0);
 //# sourceMappingURL=version.js.map
 
