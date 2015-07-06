9e2171a314b68887d537112e11d188529fc8676f
Olivier Breuleux <breuleux@gmail.com>
2015-07-06 16:05:29 -0400

    noboil = true by default with register

--- register.js
+++ register.js
@@ -2,8 +2,8 @@
 "use strict";
 
 if (process.title == "iojs") {
-    require("./lib6/register").install({es5: false, lazy: true});
+    require("./lib6/register").install({es5: false, lazy: true, noboil: true});
 }
 else {
-    require("./lib5/register").install({es5: true, lazy: true});
+    require("./lib5/register").install({es5: true, lazy: true, noboil: true});
 }
