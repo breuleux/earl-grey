6462db5ce9bcae3184d667e4aa5c466c7a720d16
Olivier Breuleux <breuleux@gmail.com>
2015-07-06 16:42:09 -0400

    replacing the noboil option by the more flexible runtime option

--- register.js
+++ register.js
@@ -2,8 +2,8 @@
 "use strict";
 
 if (process.title == "iojs") {
-    require("./lib6/register").install({es5: false, lazy: true, noboil: true});
+    require("./lib6/register").install({es5: false, lazy: true, noboil: true, runtime: null});
 }
 else {
-    require("./lib5/register").install({es5: true, lazy: true, noboil: true});
+    require("./lib5/register").install({es5: true, lazy: true, noboil: true, runtime: null});
 }
