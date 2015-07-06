e9ba7aaf52a6b32fd2321bd2754cdbb6d7181d72
Olivier Breuleux <breuleux@gmail.com>
2015-07-06 16:43:31 -0400

    purged noboil except as a cli option

--- register.js
+++ register.js
@@ -2,8 +2,8 @@
 "use strict";
 
 if (process.title == "iojs") {
-    require("./lib6/register").install({es5: false, lazy: true, noboil: true, runtime: null});
+    require("./lib6/register").install({es5: false, lazy: true, runtime: null});
 }
 else {
-    require("./lib5/register").install({es5: true, lazy: true, noboil: true, runtime: null});
+    require("./lib5/register").install({es5: true, lazy: true, runtime: null});
 }
